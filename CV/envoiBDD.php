66e7365f6b5a40346207f05fd33b0c3ca6f36bf9
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2016-09-06 09:22:12 +0200

    Update envoiBDD.php

--- CV/envoiBDD.php
+++ CV/envoiBDD.php
@@ -1,9 +1,9 @@
-<?php
-include_once 'Db.php';
-
-$db=new Db(cv);
-$db->connect("cv");
-
+<html>
+<body>
+<fieldset>
+  <?php
+mysql_connect("locaslhost","root","");
+mysql_select_db("cv");
 $Nom=$_POST['Nom'];
 $Prénom=$_POST['Prenom'];
 $Date=$_POST['date'];
@@ -12,15 +12,24 @@ $Adresse=$_POST['Address'];
 $Ville=$_POST['Ville'];
 $CP=$_POST['code'];
 $n°tel=$_POST['telephone'];
+mysql_query("INSERT INTO information (Nom,Prénom,Date,Adresse,Ville,CP,Email,n°tel) VALUES ('$Nom','$Prénom','$Date','$Adresse','$Ville','$CP','$Email','$n°tel')");
 
 
-
-echo "Votre nom est :", $Nom;
-echo "Votre prenom est :", $Prénom;
-echo "Votre date est :", $Date;
+echo "Votre Nom est :", $Nom, "et votre prénom", $Prénom;
+echo "<br></br>";
+echo "Votre date de naissance  est :", $Date;
+echo "<br></br>";
 echo "Votre nom est :", $Email;
+echo "<br></br>";
 echo "Votre nom est :", $Adresse;
+echo "<br></br>";
 echo "Votre nom est :", $Ville;
+echo "<br></br>";
 echo "Votre nom est :", $CP;
+echo "<br></br>";
 echo "Votre nom est :", $n°tel;
+echo "<br></br>";
 ?>
+</fieldset>  
+</body>
+</html>
